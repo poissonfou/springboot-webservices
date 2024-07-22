60aeb95946654825fdf155fecac14b4d57c8a58c
Emerson Lima <emersonlimago123@gmail.com>
2024-07-22 12:27:46 -0300

    Added custom exception to findById error

--- course/src/main/java/emerson/course/services/UserService.java
+++ course/src/main/java/emerson/course/services/UserService.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 
 import emerson.course.entities.User;
 import emerson.course.repositories.UserRepository;
+import emerson.course.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class UserService {
@@ -21,7 +22,7 @@ public class UserService {
 	
 	public User findById(Long id) {
 		Optional<User> obj = repository.findById(id);
-		return obj.get();
+		return obj.orElseThrow(() -> new ResourceNotFoundException(id));
 	}
 	
 	public User insert(User obj) {
