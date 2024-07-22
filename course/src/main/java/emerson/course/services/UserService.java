bf4b33373b04406eb86a66903e7407c7048bba9e
Emerson Lima <emersonlimago123@gmail.com>
2024-07-22 12:46:13 -0300

    Added custon exception handler to update

--- course/src/main/java/emerson/course/services/UserService.java
+++ course/src/main/java/emerson/course/services/UserService.java
@@ -12,6 +12,7 @@ import emerson.course.entities.User;
 import emerson.course.repositories.UserRepository;
 import emerson.course.services.exceptions.DatabaseException;
 import emerson.course.services.exceptions.ResourceNotFoundException;
+import jakarta.persistence.EntityNotFoundException;
 
 @Service
 public class UserService {
@@ -44,9 +45,14 @@ public class UserService {
 	}
 	
 	public User update(Long id, User obj) {
-		User entity = repository.getReferenceById(id);
-		updateData(entity, obj);
-		return repository.save(entity);
+		try {
+			User entity = repository.getReferenceById(id);
+			updateData(entity, obj);
+			return repository.save(entity);
+		}catch(EntityNotFoundException e) {
+			throw new ResourceNotFoundException(id);
+		}
+	
 	}
 
 	private void updateData(User entity, User obj) {
